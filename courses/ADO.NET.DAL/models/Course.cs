a9235b4ade5673d0b3b128284e029e805f148df2
maslov-maksym-serhiiovych-chnu <maslov.maksym@chnu.edu.ua>
2024-09-05 09:52:11 +0300

    restructure project and create models

---
+++ courses/ADO.NET.DAL/models/Course.cs
@@ -0,0 +1,9 @@
+﻿namespace ADO.NET.DAL.models;
+
+public class Course
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public string? Description { get; set; }
+    public ICollection<CourseUser> CourseUsers { get; set; } = new List<CourseUser>();
+}
---
+++ courses/ADO.NET.DAL/models/CourseUser.cs
@@ -0,0 +1,8 @@
+﻿namespace ADO.NET.DAL.models;
+
+public class CourseUser
+{
+    public int Id { get; set; }
+    public int CourseId { get; set; }
+    public Course Course { get; set; } = null!;
+}
