b34d65a803417acd9dfbf51593a9381e3d1a010c
maslov-maksym-serhiiovych-chnu <maslov.maksym@chnu.edu.ua>
2024-09-08 23:15:46 +0300

    fix DapperCoursesController.cs and configure repositories in Program.cs

--- Courses/PL/Program.cs
+++ Courses/PL/Program.cs
@@ -3,12 +3,13 @@ using Npgsql;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
-builder.Services.AddScoped<AdoDotNetCourseRepository>(_ =>
-{
-    string? connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-    NpgsqlConnection connection = new(connectionString);
-    return new AdoDotNetCourseRepository(connection);
-});
+
+string? connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+NpgsqlConnection connection = new(connectionString);
+builder.Services.AddScoped<AdoDotNetCourseRepository>(_ => new AdoDotNetCourseRepository(connection));
+builder.Services.AddScoped<AdoDotNetCourseUserRepository>(_ => new AdoDotNetCourseUserRepository(connection));
+builder.Services.AddScoped<DapperCourseRepository>(_ => new DapperCourseRepository(connection));
+builder.Services.AddScoped<DapperCourseUserRepository>(_ => new DapperCourseUserRepository(connection));
 
 WebApplication app = builder.Build();
 app.MapControllers();
