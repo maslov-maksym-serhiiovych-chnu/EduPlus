1c18cbb3baf534040734d9518ee03e5e781bba81
maslov-maksym-serhiiovych-chnu <maslov.maksym@chnu.edu.ua>
2024-09-08 23:07:55 +0300

    create DapperCourseUsersController.cs

---
+++ Courses/PL/Controllers/DapperCourseUsersController.cs
@@ -0,0 +1,57 @@
+﻿using DAL.Data;
+using DAL.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PL.Controllers;
+
+[ApiController]
+[Route("/api/[controller]")]
+public class DapperCourseUsersController(DapperCourseUserRepository dapperCourseUserRepository) : ControllerBase
+{
+    [HttpGet]
+    public async Task<IActionResult> GetAll()
+    {
+        var courseUsers = await dapperCourseUserRepository.GetAll();
+        return Ok(courseUsers);
+    }
+
+    [HttpGet("{id:int}")]
+    public async Task<IActionResult> GetById(int id)
+    {
+        CourseUser? courseUser = await dapperCourseUserRepository.GetById(id);
+        return courseUser == null ? NotFound() : Ok(courseUser);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create([FromBody] CourseUser courseUser)
+    {
+        await dapperCourseUserRepository.Create(courseUser);
+        return CreatedAtAction(nameof(GetById), new { courseUser.Id }, courseUser);
+    }
+
+    [HttpPut("{id:int}")]
+    public async Task<IActionResult> UpdateById(int id, [FromBody] CourseUser courseUser)
+    {
+        CourseUser? existingCourseUser = await dapperCourseUserRepository.GetById(id);
+        if (existingCourseUser == null)
+        {
+            return NotFound();
+        }
+
+        await dapperCourseUserRepository.UpdateById(id, courseUser);
+        return NoContent();
+    }
+
+    [HttpDelete("{id:int}")]
+    public async Task<IActionResult> DeleteById(int id)
+    {
+        CourseUser? existingCourseUser = await dapperCourseUserRepository.GetById(id);
+        if (existingCourseUser == null)
+        {
+            return NotFound();
+        }
+
+        await dapperCourseUserRepository.DeleteById(id);
+        return NoContent();
+    }
+}
