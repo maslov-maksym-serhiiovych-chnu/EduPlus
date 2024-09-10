399a0917a00f1284a70847f48d602261ac7789ad
maslov-maksym-serhiiovych-chnu <maslov.maksym@chnu.edu.ua>
2024-09-10 16:18:47 +0300

    update postgres version in docker-compose.yml and create Task model

---
+++ tasks/src/main/java/ua/edu/chnu/tasks/dal/models/Task.java
@@ -0,0 +1,22 @@
+package ua.edu.chnu.tasks.dal.models;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "tasks")
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "description", length = Integer.MAX_VALUE)
+    private String description;
+}
