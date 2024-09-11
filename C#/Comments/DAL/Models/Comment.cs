4a905f6e760fae5820b701da3bc18ba89fc3dc3a
maslov-maksym-serhiiovych-chnu <maslov.maksym@chnu.edu.ua>
2024-09-11 13:05:05 +0300

    create Comment.cs

---
+++ C#/Comments/DAL/Models/Comment.cs
@@ -0,0 +1,8 @@
+﻿namespace DAL.Models;
+
+public class Comment
+{
+    public int Id { get; set; }
+    public string Author { get; set; } = null!;
+    public string Content { get; set; } = null!;
+}
