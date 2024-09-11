3555849901583997af5400d1edd38a9233365e14
maslov-maksym-serhiiovych-chnu <maslov.maksym@chnu.edu.ua>
2024-09-11 14:30:09 +0300

    create Comment.java

---
+++ Java/comments/src/main/java/ua/edu/chnu/comments/models/Comment.java
@@ -0,0 +1,22 @@
+package ua.edu.chnu.comments.models;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.ColumnDefault;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "comments")
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false)
+    private String author;
+
+    @Column(nullable = false)
+    private String content;
+}
