a59cdd31026a5340253e9aa77e2bd46cef34b07b
maslov-maksym-serhiiovych-chnu <maslov.maksym@chnu.edu.ua>
2024-09-14 12:46:41 +0300

    create CoursesDbContext.cs

---
+++ C#/Courses/DAL/Data/CoursesDbContext.cs
@@ -0,0 +1,15 @@
+﻿using DAL.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DAL.Data;
+
+public class CoursesDbContext : DbContext
+{
+    public DbSet<Course> Courses { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        const string connectionString = "Host=localhost;Port=5433;Database=courses-db;Username=courses-db;Password=courses-db";
+        optionsBuilder.UseNpgsql(connectionString);
+    }
+}
