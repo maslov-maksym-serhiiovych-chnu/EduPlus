60b1c650a453d27b03a5cd1e24d4e21b326fceea
maslov-maksym-serhiiovych-chnu <maslov.maksym@chnu.edu.ua>
2024-09-17 13:21:09 +0300

    fix init-comments-db.sql

--- scripts/init-comments-db.sql
+++ scripts/init-comments-db.sql
@@ -1,12 +1,12 @@
-﻿create table if not exists comments
+﻿create table if not exists courses
+(
+    id serial primary key
+);
+
+create table if not exists comments
 (
     id        serial primary key,
     author    varchar(255)                not null,
     content   text                        not null,
     course_id int references courses (id) not null
-);
-
-create table if not exists courses
-(
-    id serial primary key
 )
