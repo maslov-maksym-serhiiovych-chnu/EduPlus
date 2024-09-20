b257357b7862bf4456576d58d2f907df5f46e1df
maslov-maksym-serhiiovych-chnu <maslov.maksym@chnu.edu.ua>
2024-09-20 09:49:34 +0300

    update service to use comment dto

--- Java/comments/src/main/java/ua/edu/chnu/comments/services/CommentService.java
+++ Java/comments/src/main/java/ua/edu/chnu/comments/services/CommentService.java
@@ -2,42 +2,78 @@ package ua.edu.chnu.comments.services;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import ua.edu.chnu.comments.dtos.CommentDTO;
 import ua.edu.chnu.comments.exceptions.CommentNotFoundByIdException;
 import ua.edu.chnu.comments.models.Comment;
 import ua.edu.chnu.comments.repositories.CommentRepository;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class CommentService {
     private final CommentRepository repository;
-    
-    public void create(Comment comment) {
+
+    public CommentDTO create(CommentDTO commentDTO) {
+        Comment comment = toModel(commentDTO);
         repository.save(comment);
+        
+        return commentDTO;
     }
-    
-    public Iterable<Comment> getAll() {
-        return repository.findAll();
+
+    public List<CommentDTO> getAll() {
+        return repository.findAll()
+                .stream()
+                .map(this::toDTO)
+                .toList();
     }
-    
-    public Comment get(int id) {
-        return repository.findById(id).orElse(null);
+
+    public CommentDTO get(int id) {
+        Comment comment = repository.findById(id).orElse(null);
+        return toDTO(comment);
     }
-    
-    public void update(int id, Comment comment) {
+
+    public void update(int id, CommentDTO commentDTO) {
         if (!repository.existsById(id)) {
             throw new CommentNotFoundByIdException(id);
         }
-        
-        Comment updated = new Comment(id, comment.getAuthor(), comment.getContent());
-        repository.save(updated);
+
+        Comment comment = toModel(commentDTO);
+        comment.setId(id);
+
+        repository.save(comment);
     }
-    
+
     public void delete(int id) {
-        Comment comment = get(id);
-        if (comment == null) {
+        CommentDTO commentDTO = get(id);
+        if (commentDTO == null) {
             throw new CommentNotFoundByIdException(id);
         }
-        
-        repository.delete(comment);
+
+        repository.delete(toModel(commentDTO));
+    }
+
+    private Comment toModel(CommentDTO commentDTO) {
+        if (commentDTO == null) {
+            return null;
+        }
+
+        Comment comment = new Comment();
+        comment.setAuthor(commentDTO.getAuthor());
+        comment.setContent(commentDTO.getContent());
+
+        return comment;
+    }
+
+    private CommentDTO toDTO(Comment comment) {
+        if (comment == null) {
+            return null;
+        }
+
+        CommentDTO commentDTO = new CommentDTO();
+        commentDTO.setAuthor(comment.getAuthor());
+        commentDTO.setContent(comment.getContent());
+
+        return commentDTO;
     }
 }
