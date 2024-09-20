428e5a4e377df8d5866dd0ff7cd34794f8722f64
maslov-maksym-serhiiovych-chnu <maslov.maksym@chnu.edu.ua>
2024-09-20 08:53:04 +0300

    remove unnecessary migration and rollback init script for comments db

--- scripts/init-comments-db.sql
+++ scripts/init-comments-db.sql
@@ -1,14 +1,6 @@
-﻿create table if not exists courses
+﻿create table if not exists comments
 (
-    id          serial primary key,
-    name        varchar(255) unique not null,
-    description text
-);
-
-create table if not exists comments
-(
-    id        serial primary key,
-    author    varchar(255)                not null,
-    content   text                        not null,
-    course_id int references courses (id) not null
+    id      serial primary key,
+    author  varchar(255) not null,
+    content text         not null
 )
