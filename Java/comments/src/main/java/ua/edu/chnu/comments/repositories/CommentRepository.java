53feacd81ca3c2f9c3bb6f9e97fa3e677e823d3e
maslov-maksym-serhiiovych-chnu <maslov.maksym@chnu.edu.ua>
2024-09-20 09:35:03 +0300

    update repository to extends JpaRepository

--- Java/comments/src/main/java/ua/edu/chnu/comments/repositories/CommentRepository.java
+++ Java/comments/src/main/java/ua/edu/chnu/comments/repositories/CommentRepository.java
@@ -1,9 +1,9 @@
 package ua.edu.chnu.comments.repositories;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ua.edu.chnu.comments.models.Comment;
 
 @Repository
-public interface CommentRepository extends CrudRepository<Comment, Integer> {
+public interface CommentRepository extends JpaRepository<Comment, Integer> {
 }
