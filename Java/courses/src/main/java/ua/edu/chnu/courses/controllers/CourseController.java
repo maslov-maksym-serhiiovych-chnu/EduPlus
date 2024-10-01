1910353f24525d8b7198642b59f11dbe5ae9ff3b
maslov-maksym-serhiiovych-chnu <maslov.maksym@chnu.edu.ua>
2024-10-01 14:13:00 +0300

    refactor course controller

--- Java/courses/src/main/java/ua/edu/chnu/courses/controllers/CourseController.java
+++ Java/courses/src/main/java/ua/edu/chnu/courses/controllers/CourseController.java
@@ -1,11 +1,9 @@
 package ua.edu.chnu.courses.controllers;
 
-
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import ua.edu.chnu.courses.dtos.CourseDTO;
 import ua.edu.chnu.courses.models.Course;
 import ua.edu.chnu.courses.services.CourseService;
 
@@ -18,69 +16,34 @@ public class CourseController {
     private final CourseService service;
 
     @GetMapping
-    public ResponseEntity<List<CourseDTO>> readAll() {
-        var courseDTOs = service.readAll()
-                .stream()
-                .map(CourseController::toDTO)
-                .toList();
-        return ResponseEntity.ok(courseDTOs);
+    public ResponseEntity<List<Course>> readAll() {
+        var courses = service.readAll();
+        return ResponseEntity.ok(courses);
     }
 
     @GetMapping("{id}")
-    public ResponseEntity<CourseDTO> read(@PathVariable int id) {
+    public ResponseEntity<Course> read(@PathVariable int id) {
         Course course = service.read(id);
-
-        CourseDTO courseDTO = toDTO(course);
-        return ResponseEntity.ok(courseDTO);
+        return ResponseEntity.ok(course);
     }
 
     @PostMapping
-    public ResponseEntity<CourseDTO> create(@RequestBody CourseDTO courseDTO) {
-        Course course = toModel(courseDTO);
-
+    public ResponseEntity<Course> create(@RequestBody Course course) {
         Course created = service.create(course);
-
-        CourseDTO createdDTO = toDTO(created);
-        return ResponseEntity.status(HttpStatus.CREATED).body(createdDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(created);
     }
 
     @PutMapping("{id}")
-    public ResponseEntity<CourseDTO> update(@PathVariable int id, @RequestBody CourseDTO courseDTO) {
-        Course course = toModel(courseDTO);
-
+    public ResponseEntity<Course> update(@PathVariable int id, @RequestBody Course course) {
         service.update(id, course);
 
         return ResponseEntity.noContent().build();
     }
 
     @DeleteMapping("{id}")
-    public ResponseEntity<CourseDTO> delete(@PathVariable int id) {
+    public ResponseEntity<Course> delete(@PathVariable int id) {
         service.delete(id);
 
         return ResponseEntity.noContent().build();
-    }
-
-    public static Course toModel(CourseDTO courseDTO) {
-        if (courseDTO == null) {
-            return null;
-        }
-
-        Course course = new Course();
-        course.setName(courseDTO.getName());
-        course.setDescription(courseDTO.getDescription());
-
-        return course;
-    }
-
-    public static CourseDTO toDTO(Course course) {
-        if (course == null) {
-            return null;
-        }
-
-        CourseDTO courseDTO = new CourseDTO();
-        courseDTO.setName(course.getName());
-        courseDTO.setDescription(course.getDescription());
-
-        return courseDTO;
     }
 }
--- Java/courses/src/main/java/ua/edu/chnu/courses/dtos/CourseDTO.java
+++
@@ -1,8 +0,0 @@
-package ua.edu.chnu.courses.dtos;
-
-import lombok.Data;
-
-@Data
-public class CourseDTO {
-    private String name, description;
-}
