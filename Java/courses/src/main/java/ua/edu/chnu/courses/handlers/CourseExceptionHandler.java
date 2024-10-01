4dd2cbce56ce47acf8f625b6e31220047c307172
maslov-maksym-serhiiovych-chnu <maslov.maksym@chnu.edu.ua>
2024-10-01 14:12:03 +0300

    add logging to course exception handler and refacto

--- Java/courses/src/main/java/ua/edu/chnu/courses/handlers/CourseExceptionHandler.java
+++ Java/courses/src/main/java/ua/edu/chnu/courses/handlers/CourseExceptionHandler.java
@@ -1,21 +1,19 @@
 package ua.edu.chnu.courses.handlers;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import ua.edu.chnu.courses.exceptions.CourseNotFoundByIdException;
 
-import java.util.HashMap;
-import java.util.Map;
-
+@Slf4j
 @ControllerAdvice
 public class CourseExceptionHandler {
     @ExceptionHandler(CourseNotFoundByIdException.class)
-    public ResponseEntity<Map<String, String>> handleCourseNotFoundByIdException(CourseNotFoundByIdException exception) {
-        var errors = new HashMap<String, String>();
-        errors.put("error", exception.getMessage());
+    public ResponseEntity<String> handleCourseNotFoundByIdException(CourseNotFoundByIdException exception) {
+        log.error(exception.getMessage());
 
-        return new ResponseEntity<>(errors, HttpStatus.NOT_FOUND);
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(exception.getMessage());
     }
 }
