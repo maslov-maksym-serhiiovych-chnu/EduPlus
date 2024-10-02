3316f853a9cbee1be6be47a3cff9fdac0e9842a7
maslov-maksym-serhiiovych-chnu <maslov.maksym@chnu.edu.ua>
2024-10-02 18:43:46 +0300

    comments api csharp: create comment service interface

---
+++ comments_api_csharp/BLL/Services/ICommentService.cs
@@ -0,0 +1,12 @@
+﻿using DAL.Models;
+
+namespace BLL.Services;
+
+public interface ICommentService
+{
+    Task<int> CreateAsync(Comment comment);
+    Task<IEnumerable<Comment>> ReadAllAsync();
+    Task<Comment> ReadAsync(int id);
+    Task UpdateAsync(int id, Comment comment);
+    Task DeleteAsync(int id);
+}
