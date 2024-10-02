b6f01319b4f376878bff7b6d145b117f461df9c6
maslov-maksym-serhiiovych-chnu <maslov.maksym@chnu.edu.ua>
2024-10-02 18:47:40 +0300

    comments api csharp: fix comment not found exception

--- comments_api_csharp/BLL/Exceptions/CommentNotFoundException.cs
+++ comments_api_csharp/BLL/Exceptions/CommentNotFoundException.cs
@@ -1,3 +1,3 @@
 ﻿namespace BLL.Exceptions;
 
-public class CommentNotFoundException(string? message) : Exception(message);
+public class CommentNotFoundException(string message) : Exception(message);
