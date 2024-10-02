8cd46ebca0823a8f6c10b4d20cb4d2258a01e42d
maslov-maksym-serhiiovych-chnu <maslov.maksym@chnu.edu.ua>
2024-10-03 02:54:21 +0300

    Courses: create Course model

---
+++ api/Courses/DAL/Models/Course.cs
@@ -0,0 +1,8 @@
+﻿namespace DAL.Models;
+
+public class Course
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = null!;
+    public string Description { get; set; } = null!;
+}
