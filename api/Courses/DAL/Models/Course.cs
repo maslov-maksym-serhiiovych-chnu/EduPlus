8ba9e7b1a3ebd32a03b705653c00bc7aced6c20e
maslov-maksym-serhiiovych-chnu <maslov.maksym@chnu.edu.ua>
2024-10-03 03:01:43 +0300

    Courses: fix Course model

--- api/Courses/DAL/Models/Course.cs
+++ api/Courses/DAL/Models/Course.cs
@@ -4,5 +4,5 @@ public class Course
 {
     public int Id { get; set; }
     public string Name { get; set; } = null!;
-    public string Description { get; set; } = null!;
+    public string? Description { get; set; }
 }
