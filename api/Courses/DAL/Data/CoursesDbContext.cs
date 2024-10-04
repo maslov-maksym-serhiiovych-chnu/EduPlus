2ab7076aae1c7dbc4f8b5ed8d06152800d159e40
maslov-maksym-serhiiovych-chnu <maslov.maksym@chnu.edu.ua>
2024-10-04 11:26:19 +0300

    Courses: create courses db context

---
+++ api/Courses/DAL/Data/CoursesDbContext.cs
@@ -0,0 +1,31 @@
+﻿using DAL.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DAL.Data;
+
+public class CoursesDbContext(DbContextOptions<CoursesDbContext> options) : DbContext(options)
+{
+    public DbSet<Course> Courses { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Course>().ToTable("courses");
+
+        modelBuilder.Entity<Course>().HasKey(c => c.Id);
+
+        modelBuilder.Entity<Course>()
+            .Property(c => c.Id)
+            .ValueGeneratedOnAdd();
+
+        modelBuilder.Entity<Course>()
+            .Property(c => c.Name)
+            .HasColumnName("name")
+            .HasColumnType("VARCHAR(255)")
+            .IsRequired();
+
+        modelBuilder.Entity<Course>()
+            .Property(c => c.Description)
+            .HasColumnName("description")
+            .HasColumnType("TEXT");
+    }
+}
