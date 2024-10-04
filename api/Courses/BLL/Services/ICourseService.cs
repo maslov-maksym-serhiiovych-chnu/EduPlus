0ee10595d8fafeb8b7efe27e7f626f6627874521
maslov-maksym-serhiiovych-chnu <maslov.maksym@chnu.edu.ua>
2024-10-04 14:14:36 +0300

    Courses: add suffix Async to course service interface methods

--- api/Courses/BLL/Services/ICourseService.cs
+++ api/Courses/BLL/Services/ICourseService.cs
@@ -4,9 +4,9 @@ namespace BLL.Services;
 
 public interface ICourseService
 {
-    Task<int> Create(Course course);
-    Task<IEnumerable<Course>> ReadAll();
-    Task<Course?> Read(int id);
-    Task Update(int id, Course course);
-    Task Delete(int id);
+    Task<int> CreateAsync(Course course);
+    Task<IEnumerable<Course>> ReadAllAsync();
+    Task<Course> ReadAsync(int id);
+    Task UpdateAsync(int id, Course course);
+    Task DeleteAsync(int id);
 }
