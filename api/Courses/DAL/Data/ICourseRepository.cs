d890f77775af89fbae0ae7e8a177e753eae31a02
maslov-maksym-serhiiovych-chnu <maslov.maksym@chnu.edu.ua>
2024-10-04 14:12:55 +0300

    Courses: create course repository interface

---
+++ api/Courses/DAL/Data/ICourseRepository.cs
@@ -0,0 +1,12 @@
+﻿using DAL.Models;
+
+namespace DAL.Data;
+
+public interface ICourseRepository
+{
+    Task<int> CreateAsync(Course course);
+    Task<IEnumerable<Course>> ReadAllAsync();
+    Task<Course?> ReadAsync(int id);
+    Task UpdateAsync(int id, Course course);
+    Task DeleteAsync(int id);
+}
