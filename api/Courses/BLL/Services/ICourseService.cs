000cd3503c56bb883d2f1527743aa69b75c75f1a
maslov-maksym-serhiiovych-chnu <maslov.maksym@chnu.edu.ua>
2024-10-04 11:33:49 +0300

    Courses: create course service interface

---
+++ api/Courses/BLL/Services/ICourseService.cs
@@ -0,0 +1,12 @@
+﻿using DAL.Models;
+
+namespace BLL.Services;
+
+public interface ICourseService
+{
+    Task<int> Create(Course course);
+    Task<IEnumerable<Course>> ReadAll();
+    Task<Course?> Read(int id);
+    Task Update(int id, Course course);
+    Task Delete(int id);
+}
