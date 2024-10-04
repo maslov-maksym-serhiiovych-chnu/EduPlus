b4398c4aca472427b04cff855d3b259242f25a37
maslov-maksym-serhiiovych-chnu <maslov.maksym@chnu.edu.ua>
2024-10-04 18:34:10 +0300

    Courses: create exception handler middleware

---
+++ api/Courses/PL/Middlewares/ExceptionHandlerMiddleware.cs
@@ -0,0 +1,22 @@
+﻿using BLL.Exceptions;
+
+namespace PL.Middlewares;
+
+public class ExceptionHandlerMiddleware(RequestDelegate next, ILogger<ExceptionHandlerMiddleware> logger)
+{
+    public async Task InvokeAsync(HttpContext context)
+    {
+        try
+        {
+            await next(context);
+        }
+        catch (CourseNotFoundException exception)
+        {
+            string message = exception.Message;
+            context.Response.StatusCode = StatusCodes.Status404NotFound;
+            await context.Response.WriteAsJsonAsync(message);
+
+            logger.LogError(message);
+        }
+    }
+}
