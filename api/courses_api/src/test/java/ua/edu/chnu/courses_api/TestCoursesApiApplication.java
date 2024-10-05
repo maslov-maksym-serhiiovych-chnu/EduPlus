e76c375a99ccbd2dd8cf8a7b93d7eb41e1056435
maslov-maksym-serhiiovych-chnu <maslov.maksym@chnu.edu.ua>
2024-10-05 21:03:08 +0300

    courses_api: configure tests

--- api/courses_api/src/test/java/ua/edu/chnu/courses_api/TestCoursesApiApplication.java
+++ api/courses_api/src/test/java/ua/edu/chnu/courses_api/TestCoursesApiApplication.java
@@ -3,9 +3,7 @@ package ua.edu.chnu.courses_api;
 import org.springframework.boot.SpringApplication;
 
 public class TestCoursesApiApplication {
-
     public static void main(String[] args) {
         SpringApplication.from(CoursesApiApplication::main).with(TestcontainersConfiguration.class).run(args);
     }
-
 }
