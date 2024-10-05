731d77b1b09c044a6def762137894aebaa0a867a
maslov-maksym-serhiiovych-chnu <maslov.maksym@chnu.edu.ua>
2024-10-05 19:00:10 +0300

    courses_api: create course model

---
+++ api/courses_api/src/main/java/ua/edu/chnu/courses_api/courses/Course.java
@@ -0,0 +1,24 @@
+package ua.edu.chnu.courses_api.courses;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "courses")
+public class Course {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "description", nullable = false, length = Integer.MAX_VALUE)
+    private String description;
+}
