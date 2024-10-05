2f9ecfb70fee7d263aa234f532c7e729758b3866
maslov-maksym-serhiiovych-chnu <maslov.maksym@chnu.edu.ua>
2024-10-05 21:03:38 +0300

    courses_api: create course exception handler

---
+++ api/courses_api/src/main/java/ua/edu/chnu/courses_api/courses/CourseExceptionHandler.java
@@ -0,0 +1,18 @@
+package ua.edu.chnu.courses_api.courses;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@Slf4j
+@ControllerAdvice
+public class CourseExceptionHandler {
+    @ExceptionHandler(CourseNotFoundException.class)
+    public ResponseEntity<String> handleCourseNotFound(CourseNotFoundException exception) {
+        String message = exception.getMessage();
+        log.error(message);
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(message);
+    }
+}
