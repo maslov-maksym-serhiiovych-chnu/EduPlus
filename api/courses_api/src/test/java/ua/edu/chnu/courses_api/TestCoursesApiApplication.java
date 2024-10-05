138b2d56e8fe5eec2e36d4743aa558dd6d09532b
maslov-maksym-serhiiovych-chnu <maslov.maksym@chnu.edu.ua>
2024-10-05 18:48:54 +0300

    courses_api: initial commit

---
+++ api/courses_api/src/test/java/ua/edu/chnu/courses_api/TestCoursesApiApplication.java
@@ -0,0 +1,11 @@
+package ua.edu.chnu.courses_api;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestCoursesApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.from(CoursesApiApplication::main).with(TestcontainersConfiguration.class).run(args);
+    }
+
+}
