82c5af4804fcf1d3184ea57ac326a114188e0616
maslov-maksym-serhiiovych-chnu <maslov.maksym@chnu.edu.ua>
2024-10-06 12:29:43 +0300

    comments_api: add annotation for feign clients to main class

--- api/comments_api/src/main/java/ua/edu/chnu/comments_api/CommentsApiApplication.java
+++ api/comments_api/src/main/java/ua/edu/chnu/comments_api/CommentsApiApplication.java
@@ -2,8 +2,10 @@ package ua.edu.chnu.comments_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class CommentsApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CommentsApiApplication.class, args);
