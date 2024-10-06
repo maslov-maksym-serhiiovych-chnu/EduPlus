1e46fcf581e0f817875bfdab799d3c0d22dc3954
maslov-maksym-serhiiovych-chnu <maslov.maksym@chnu.edu.ua>
2024-10-06 10:50:13 +0300

    courses_api: refactor controller integration test

--- api/courses_api/src/test/java/ua/edu/chnu/courses_api/CourseControllerIntegrationTest.java
+++ api/courses_api/src/test/java/ua/edu/chnu/courses_api/CourseControllerIntegrationTest.java
@@ -3,131 +3,119 @@ package ua.edu.chnu.courses_api;
 import io.restassured.RestAssured;
 import jakarta.annotation.PostConstruct;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.HttpStatus;
-import org.testcontainers.containers.PostgreSQLContainer;
 import org.hamcrest.Matchers;
 import ua.edu.chnu.courses_api.courses.Course;
-
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
 
 @Import(TestcontainersConfiguration.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class CourseControllerIntegrationTest {
-    private static final int READ_ID = 1, UPDATED_ID = 2, DELETED_ID = 3, NOT_FOUND_ID = Integer.MAX_VALUE;
-    private static final String NOT_FOUND_MESSAGE = "course not found by id: " + NOT_FOUND_ID,
-            DELETED_NOT_FOUND_MESSAGE = "course not found by id: " + DELETED_ID;
+    private static final int READ_ID = 1, SHOULD_UPDATED_ID = 2, DELETED_ID = 3, NOT_FOUND_ID = Integer.MAX_VALUE;
+    
+    private static final String DELETED_COURSE_NOT_FOUND_MESSAGE = "course not found by id: " + DELETED_ID,
+            NOT_FOUND_MESSAGE = "course not found by id: " + NOT_FOUND_ID;
 
-    private static final Course READ = new Course(READ_ID, "read", "read"),
-            CREATED = new Course(0, "created", "created"),
-            UPDATED = new Course(UPDATED_ID, "updated", "updated");
-
-    private static final String TEST_DATA = """
-            INSERT INTO courses (name, description) VALUES ('read', 'read'),
-                           ('should-updated', 'should-updated'),
-                           ('deleted', 'deleted')
-            """;
-
-    private static boolean testDataInserted;
+    private static final Course READ_COURSE = new Course(0, "read", "read"),
+            SHOULD_UPDATED_COURSE = new Course(0, "should-updated", "should-updated"),
+            DELETED_COURSE = new Course(0, "deleted", "deleted"),
+            CREATED_COURSE = new Course(0, "created", "created"),
+            UPDATED_COURSE = new Course(0, "updated", "updated");
+    
+    private static boolean testCoursesPosted;
 
     @LocalServerPort
     private int port;
 
-    @Autowired
-    private PostgreSQLContainer<?> container;
-
     private String url;
 
     @PostConstruct
-    public void init() throws SQLException {
+    void init() {
         url = "http://localhost:" + port + "/api/courses";
 
-        if (testDataInserted) {
+        if (testCoursesPosted) {
             return;
         }
 
-        try (Connection connection = DriverManager.getConnection(
-                container.getJdbcUrl(),
-                container.getUsername(),
-                container.getPassword()
-        ); Statement statement = connection.createStatement()) {
-            statement.execute(TEST_DATA);
-        }
+        RestAssured.given()
+                .contentType("application/json")
+                .body(READ_COURSE)
+                .post(url);
 
-        testDataInserted = true;
+        RestAssured.given()
+                .contentType("application/json")
+                .body(SHOULD_UPDATED_COURSE)
+                .post(url);
+
+        RestAssured.given()
+                .contentType("application/json")
+                .body(DELETED_COURSE)
+                .post(url);
+
+        testCoursesPosted = true;
     }
 
     @Test
     void testReadAll() {
         RestAssured.get(url)
                 .then()
+                .body(Matchers.notNullValue())
                 .body("[0]", Matchers.notNullValue())
                 .body("[0].id", Matchers.equalTo(READ_ID))
-                .body("[0].name", Matchers.equalTo(READ.getName()))
-                .body("[0].description", Matchers.equalTo(READ.getDescription()))
+                .body("[0].name", Matchers.equalTo(READ_COURSE.getName()))
+                .body("[0].description", Matchers.equalTo(READ_COURSE.getDescription()))
                 .statusCode(HttpStatus.OK.value());
-    }
-
-    @Test
-    void testRead() {
-        RestAssured.get(url + "/" + READ_ID)
-                .then()
-                .body(Matchers.notNullValue())
-                .body("id", Matchers.equalTo(READ.getId()))
-                .body("name", Matchers.equalTo(READ.getName()))
-                .body("description", Matchers.equalTo(READ.getDescription()))
-                .statusCode(HttpStatus.OK.value());
-    }
-
-    @Test
-    void testReadNotFound() {
-        RestAssured.get(url + "/" + NOT_FOUND_ID)
-                .then()
-                .body(Matchers.notNullValue())
-                .body(Matchers.is(NOT_FOUND_MESSAGE))
-                .statusCode(HttpStatus.NOT_FOUND.value());
     }
 
     @Test
     void testCreate() {
-        RestAssured.given()
+        String idResponse = RestAssured.given()
                 .contentType("application/json")
-                .body(CREATED)
+                .body(CREATED_COURSE)
                 .post(url)
                 .then()
-                .body("name", Matchers.equalTo(CREATED.getName()))
-                .body("description", Matchers.equalTo(CREATED.getDescription()))
-                .statusCode(HttpStatus.CREATED.value());
+                .body(Matchers.notNullValue())
+                .body("name", Matchers.equalTo(CREATED_COURSE.getName()))
+                .body("description", Matchers.equalTo(CREATED_COURSE.getDescription()))
+                .statusCode(HttpStatus.CREATED.value())
+                .extract()
+                .jsonPath()
+                .getString("id");
+
+        int id = Integer.parseInt(idResponse);
+        RestAssured.get(url + "/" + id)
+                .then()
+                .body(Matchers.notNullValue())
+                .body("id", Matchers.equalTo(id))
+                .body("name", Matchers.equalTo(CREATED_COURSE.getName()))
+                .body("description", Matchers.equalTo(CREATED_COURSE.getDescription()))
+                .statusCode(HttpStatus.OK.value());
     }
 
     @Test
     void testUpdate() {
         RestAssured.given()
                 .contentType("application/json")
-                .body(UPDATED)
-                .put(url + "/" + UPDATED_ID)
+                .body(UPDATED_COURSE)
+                .put(url + "/" + SHOULD_UPDATED_ID)
                 .then()
                 .statusCode(HttpStatus.NO_CONTENT.value());
 
-        RestAssured.get(url + "/" + UPDATED_ID)
+        RestAssured.get(url + "/" + SHOULD_UPDATED_ID)
                 .then()
                 .body(Matchers.notNullValue())
-                .body("id", Matchers.equalTo(UPDATED_ID))
-                .body("name", Matchers.equalTo(UPDATED.getName()))
-                .body("description", Matchers.equalTo(UPDATED.getDescription()));
+                .body("id", Matchers.equalTo(SHOULD_UPDATED_ID))
+                .body("name", Matchers.equalTo(UPDATED_COURSE.getName()))
+                .body("description", Matchers.equalTo(UPDATED_COURSE.getDescription()));
     }
 
     @Test
     void testUpdateNotFound() {
         RestAssured.given()
                 .contentType("application/json")
-                .body(UPDATED)
+                .body(UPDATED_COURSE)
                 .put(url + "/" + NOT_FOUND_ID)
                 .then()
                 .body(Matchers.notNullValue())
@@ -144,7 +132,7 @@ class CourseControllerIntegrationTest {
         RestAssured.get(url + "/" + DELETED_ID)
                 .then()
                 .body(Matchers.notNullValue())
-                .body(Matchers.is(DELETED_NOT_FOUND_MESSAGE))
+                .body(Matchers.is(DELETED_COURSE_NOT_FOUND_MESSAGE))
                 .statusCode(HttpStatus.NOT_FOUND.value());
     }
 
