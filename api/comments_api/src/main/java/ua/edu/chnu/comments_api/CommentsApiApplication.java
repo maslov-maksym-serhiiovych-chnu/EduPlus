4498a3313b65a276e2e5157bbf32d721e9ae14ce
maslov-maksym-serhiiovych-chnu <maslov.maksym@chnu.edu.ua>
2024-10-06 13:19:42 +0300

    comments_api: add required annotation for comment repository

--- api/comments_api/src/main/java/ua/edu/chnu/comments_api/CommentsApiApplication.java
+++ api/comments_api/src/main/java/ua/edu/chnu/comments_api/CommentsApiApplication.java
@@ -3,8 +3,10 @@ package ua.edu.chnu.comments_api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@EnableMongoRepositories
 @EnableFeignClients
 public class CommentsApiApplication {
 	public static void main(String[] args) {
