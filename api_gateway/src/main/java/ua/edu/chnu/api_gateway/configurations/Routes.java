213261f53108704b8bad0258966bca4d872a715b
maslov-maksym-serhiiovych-chnu <maslov.maksym@chnu.edu.ua>
2024-10-08 13:05:23 +0300

    api_gateway: create routes configuration

---
+++ api_gateway/src/main/java/ua/edu/chnu/api_gateway/configurations/Routes.java
@@ -0,0 +1,26 @@
+package ua.edu.chnu.api_gateway.configurations;
+
+import org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions;
+import org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.function.RequestPredicates;
+import org.springframework.web.servlet.function.RouterFunction;
+import org.springframework.web.servlet.function.ServerResponse;
+
+@Configuration(proxyBeanMethods = false)
+public class Routes {
+    @Bean
+    public RouterFunction<ServerResponse> coursesApiRoute() {
+        return GatewayRouterFunctions.route("courses-api")
+                .route(RequestPredicates.path("/api/courses"), HandlerFunctions.http("http://localhost:8081"))
+                .build();
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> commentsApiRoute() {
+        return GatewayRouterFunctions.route("comments-api")
+                .route(RequestPredicates.path("/api/comments"), HandlerFunctions.http("http://localhost:8082"))
+                .build();
+    }
+}
