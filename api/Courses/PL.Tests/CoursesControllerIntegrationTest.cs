8ad0bf4867211bf3fdadd59d7b2f8caa54f62141
maslov-maksym-serhiiovych-chnu <maslov.maksym@chnu.edu.ua>
2024-10-09 13:39:36 +0300

    Courses: refactor courses controller integration test

--- api/Courses/PL.Tests/CoursesControllerIntegrationTest.cs
+++ api/Courses/PL.Tests/CoursesControllerIntegrationTest.cs
@@ -6,30 +6,30 @@ using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Newtonsoft.Json;
-using Npgsql;
 using Testcontainers.PostgreSql;
+using StringContent = System.Net.Http.StringContent;
 
 namespace PL.Tests;
 
 public class CoursesControllerIntegrationTest : IClassFixture<WebApplicationFactory<Program>>, IAsyncLifetime
 {
-    private const int ReadId = 1, UpdatedId = 2, DeletedId = 3, NotFoundId = int.MaxValue;
-    private static readonly string TestNotFoundMessage = "course not found by id: " + NotFoundId;
-
-    private const string TestData = """
-                                    INSERT INTO courses (name, description) VALUES ('read', 'read'),
-                                                                                   ('should-updated', 'should-updated'),
-                                                                                   ('deleted', 'deleted')
-                                    """;
-
-    private static bool _testDataInitialized;
-
-    private static readonly Course Read = new() { Name = "read", Description = "read" },
-        Created = new() { Name = "created", Description = "created" },
-        Updated = new() { Name = "updated", Description = "updated" };
-
     private static readonly PostgreSqlContainer Container = new PostgreSqlBuilder().WithImage("postgres").Build();
+
     private const string Url = "api/Courses";
+
+    private const int NotFoundCourseId = int.MaxValue;
+    private static int _shouldUpdatedCourseId, _deletedCourseId;
+
+    private static readonly string NotFoundCourseMessage = "course not found by id: " + NotFoundCourseId;
+    private static string _deletedCourseNotFoundMessage = null!;
+
+    private static bool _testDataCreated;
+
+    private static readonly Course ReadCourse = new() { Name = "read", Description = "read" },
+        ShouldUpdatedCourse = new() { Name = "should-updated", Description = "should-updated" },
+        DeletedCourse = new() { Name = "deleted", Description = "deleted" },
+        CreatedCourse = new() { Name = "created", Description = "created" },
+        UpdatedCourse = new() { Name = "updated", Description = "updated" };
 
     private HttpClient _client = null!;
     private WebApplicationFactory<Program> _factory = null!;
@@ -54,17 +54,19 @@ public class CoursesControllerIntegrationTest : IClassFixture<WebApplicationFact
         CoursesDbContext context = scope.ServiceProvider.GetRequiredService<CoursesDbContext>();
         await context.Database.MigrateAsync();
 
-        if (_testDataInitialized)
+        if (_testDataCreated)
         {
             return;
         }
 
-        await using NpgsqlDataSource dataSource = NpgsqlDataSource.Create(connectionString);
-        await using NpgsqlConnection connection = await dataSource.OpenConnectionAsync();
-        await using NpgsqlCommand command = new(TestData, connection);
-        command.ExecuteNonQuery();
+        await CreateAsync(ReadCourse);
+        Course? shouldUpdatedCourse = await CreateAsync(ShouldUpdatedCourse);
+        _shouldUpdatedCourseId = shouldUpdatedCourse?.Id ?? -1;
+        Course? deletedCourse = await CreateAsync(DeletedCourse);
+        _deletedCourseId = deletedCourse?.Id ?? -1;
+        _deletedCourseNotFoundMessage = "course not found by id: " + _deletedCourseId;
 
-        _testDataInitialized = true;
+        _testDataCreated = true;
     }
 
     public Task DisposeAsync()
@@ -82,82 +84,58 @@ public class CoursesControllerIntegrationTest : IClassFixture<WebApplicationFact
         var courses = JsonConvert.DeserializeObject<Course[]>(content);
         Assert.NotNull(courses);
         Assert.NotEmpty(courses);
-        AssertCourse(Read, courses[0]);
-    }
-
-    [Fact]
-    public async Task TestRead()
-    {
-        HttpResponseMessage response = await _client.GetAsync(Url + "/" + ReadId);
-        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
-
-        string content = await response.Content.ReadAsStringAsync();
-        Course? course = JsonConvert.DeserializeObject<Course>(content);
-        AssertCourse(Read, course);
-    }
-
-    [Fact]
-    public async Task TestReadNotFound()
-    {
-        HttpResponseMessage response = await _client.GetAsync(Url + "/" + NotFoundId);
-        Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
-
-        string content = await response.Content.ReadAsStringAsync();
-        string? errorMessage = JsonConvert.DeserializeObject<string>(content);
-        Assert.Equal(TestNotFoundMessage, errorMessage);
+        AssertCourse(ReadCourse, courses[0]);
     }
 
     [Fact]
     public async Task TestCreate()
     {
-        string courseJson = JsonConvert.SerializeObject(Created);
-        HttpContent content = new StringContent(courseJson, Encoding.UTF8, "application/json");
-        HttpResponseMessage response = await _client.PostAsync(Url, content);
-        Assert.Equal(HttpStatusCode.Created, response.StatusCode);
+        Course? created = await CreateAsync(CreatedCourse);
+        AssertCourse(CreatedCourse, created);
 
-        string responseContent = await response.Content.ReadAsStringAsync();
-        Course? course = JsonConvert.DeserializeObject<Course>(responseContent);
-        AssertCourse(Created, course);
+        int id = created?.Id ?? -1;
+        await AssertReadAsync(CreatedCourse, id);
     }
 
     [Fact]
     public async Task TestUpdate()
     {
-        string courseJson = JsonConvert.SerializeObject(Updated);
-        HttpContent content = new StringContent(courseJson, Encoding.UTF8, "application/json");
-        HttpResponseMessage response = await _client.PutAsync(Url + "/" + UpdatedId, content);
-        Assert.Equal(HttpStatusCode.NoContent, response.StatusCode);
+        string updatedCourseJson = JsonConvert.SerializeObject(UpdatedCourse);
+        HttpContent updatedCourseContent = new StringContent(updatedCourseJson, Encoding.UTF8, "application/json");
+        HttpResponseMessage updatedCourseResponse = await _client.PutAsync(
+            Url + "/" + _shouldUpdatedCourseId, updatedCourseContent
+        );
+        Assert.Equal(HttpStatusCode.NoContent, updatedCourseResponse.StatusCode);
+        await AssertReadAsync(UpdatedCourse, _shouldUpdatedCourseId);
     }
 
     [Fact]
     public async Task TestUpdateNotFound()
     {
-        string courseJson = JsonConvert.SerializeObject(Updated);
+        string courseJson = JsonConvert.SerializeObject(UpdatedCourse);
         HttpContent content = new StringContent(courseJson, Encoding.UTF8, "application/json");
-        HttpResponseMessage response = await _client.PutAsync(Url + "/" + NotFoundId, content);
+        HttpResponseMessage response = await _client.PutAsync(Url + "/" + NotFoundCourseId, content);
         Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
-
-        string responseContent = await response.Content.ReadAsStringAsync();
-        string? errorMessage = JsonConvert.DeserializeObject<string>(responseContent);
-        Assert.Equal(TestNotFoundMessage, errorMessage);
+        await AssertNotFoundAsync(NotFoundCourseMessage, response);
     }
 
     [Fact]
     public async Task TestDelete()
     {
-        HttpResponseMessage response = await _client.DeleteAsync(Url + "/" + DeletedId);
-        Assert.Equal(HttpStatusCode.NoContent, response.StatusCode);
+        HttpResponseMessage deletedCourseResponse = await _client.DeleteAsync(Url + "/" + _deletedCourseId);
+        Assert.Equal(HttpStatusCode.NoContent, deletedCourseResponse.StatusCode);
+
+        HttpResponseMessage response = await _client.GetAsync(Url + "/" + _deletedCourseId);
+        Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
+        await AssertNotFoundAsync(_deletedCourseNotFoundMessage, response);
     }
 
     [Fact]
     public async Task TestDeleteNotFound()
     {
-        HttpResponseMessage response = await _client.DeleteAsync(Url + "/" + NotFoundId);
+        HttpResponseMessage response = await _client.DeleteAsync(Url + "/" + NotFoundCourseId);
         Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
-
-        string responseContent = await response.Content.ReadAsStringAsync();
-        string? message = JsonConvert.DeserializeObject<string>(responseContent);
-        Assert.Equal(TestNotFoundMessage, message);
+        await AssertNotFoundAsync(NotFoundCourseMessage, response);
     }
 
     private static void AssertCourse(Course expected, Course? actual)
@@ -165,5 +143,31 @@ public class CoursesControllerIntegrationTest : IClassFixture<WebApplicationFact
         Assert.NotNull(actual);
         Assert.Equal(expected.Name, actual.Name);
         Assert.Equal(expected.Description, actual.Description);
+    }
+
+    private async Task AssertReadAsync(Course expected, int id)
+    {
+        HttpResponseMessage response = await _client.GetAsync(Url + "/" + id);
+        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        string content = await response.Content.ReadAsStringAsync();
+        Course? course = JsonConvert.DeserializeObject<Course>(content);
+        AssertCourse(expected, course);
+    }
+
+    private static async Task AssertNotFoundAsync(string errorMessage, HttpResponseMessage response)
+    {
+        string responseContent = await response.Content.ReadAsStringAsync();
+        string? responseErrorMessage = JsonConvert.DeserializeObject<string>(responseContent);
+        Assert.Equal(errorMessage, responseErrorMessage);
+    }
+
+    private async Task<Course?> CreateAsync(Course course)
+    {
+        string courseJson = JsonConvert.SerializeObject(course);
+        HttpContent courseContent = new StringContent(courseJson, Encoding.UTF8, "application/json");
+        HttpResponseMessage courseResponse = await _client.PostAsync(Url, courseContent);
+        string courseStr = await courseResponse.Content.ReadAsStringAsync();
+        Course? created = JsonConvert.DeserializeObject<Course>(courseStr);
+        return created;
     }
 }
