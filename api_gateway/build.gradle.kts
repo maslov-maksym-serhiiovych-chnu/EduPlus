b485cb00af4ddfe72eae5f1ff49965279df6baf5
maslov-maksym-serhiiovych-chnu <maslov.maksym@chnu.edu.ua>
2024-10-11 16:32:54 +0300

    api_gateway: add dependencies for swagger doc

--- api_gateway/build.gradle.kts
+++ api_gateway/build.gradle.kts
@@ -21,6 +21,8 @@ extra["springCloudVersion"] = "2023.0.3"
 
 dependencies {
 	implementation("org.springframework.cloud:spring-cloud-starter-gateway-mvc")
+	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.6.0")
+	implementation("org.springdoc:springdoc-openapi-starter-webmvc-api:2.6.0")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
