4bc0ae780e4948e529c62fa42e26b00f7fa70401
maslov-maksym-serhiiovych-chnu <maslov.maksym@chnu.edu.ua>
2024-10-12 14:04:34 +0300

    Comments: create comment repository

---
+++ api/Comments/DAL/Data/CommentRepository.cs
@@ -0,0 +1,63 @@
+﻿using DAL.Models;
+using Dapper;
+using Microsoft.Data.Sqlite;
+
+namespace DAL.Data;
+
+public class CommentRepository(string connectionString)
+{
+    public async Task InitializeAsync()
+    {
+        await using SqliteConnection connection = new(connectionString);
+        await connection.OpenAsync();
+        const string sql = "CREATE TABLE IF NOT EXISTS comments (id INTEGER PRIMARY KEY AUTOINCREMENT, content TEXT)";
+        await connection.ExecuteAsync(sql);
+    }
+
+    public async Task<int> CreateAsync(Comment comment)
+    {
+        await using SqliteConnection connection = new(connectionString);
+        await connection.OpenAsync();
+        const string sql = "INSERT INTO comments (content) VALUES (@content)";
+        object parameter = new { comment.Content };
+        int id = await connection.ExecuteScalarAsync<int>(sql, parameter);
+        return id;
+    }
+
+    public async Task<IEnumerable<Comment>> ReadAllAsync()
+    {
+        await using SqliteConnection connection = new(connectionString);
+        await connection.OpenAsync();
+        const string sql = "SELECT * FROM comments";
+        var comments = await connection.QueryAsync<Comment>(sql);
+        return comments;
+    }
+
+    public async Task<Comment?> ReadAsync(int id)
+    {
+        await using SqliteConnection connection = new(connectionString);
+        await connection.OpenAsync();
+        const string sql = "SELECT * FROM comments WHERE id = @id";
+        object parameter = new { id };
+        Comment? comment = await connection.QuerySingleOrDefaultAsync<Comment>(sql, parameter);
+        return comment;
+    }
+
+    public async Task UpdateAsync(int id, Comment comment)
+    {
+        await using SqliteConnection connection = new(connectionString);
+        await connection.OpenAsync();
+        const string sql = "UPDATE comments SET content = @content WHERE id = @id";
+        object parameters = new { comment.Content, Id = id };
+        await connection.ExecuteAsync(sql, parameters);
+    }
+
+    public async Task DeleteAsync(int id)
+    {
+        await using SqliteConnection connection = new(connectionString);
+        await connection.OpenAsync();
+        const string sql = "DELETE FROM comments WHERE id = @id";
+        object parameter = new { id };
+        await connection.ExecuteAsync(sql, parameter);
+    }
+}
