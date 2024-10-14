7d34132d7b1dd58014bdb0653d429f30b7084af8
maslov-maksym-serhiiovych-chnu <maslov.maksym@chnu.edu.ua>
2024-10-14 10:22:40 +0300

    Comments: create comments controller

---
+++ api/Comments/PL/Controllers/CommentsController.cs
@@ -0,0 +1,55 @@
+﻿using BLL.Services;
+using DAL.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PL.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class CommentsController(CommentService service) : ControllerBase
+{
+    [HttpGet]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    public async Task<ActionResult<IEnumerable<Comment>>> ReadAll()
+    {
+        var comments = await service.ReadAllAsync();
+        return Ok(comments);
+    }
+
+    [HttpGet("{id:int}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<ActionResult<Comment>> Read(int id)
+    {
+        Comment comment = await service.ReadAsync(id);
+        return Ok(comment);
+    }
+
+    [HttpPost]
+    [ProducesResponseType(StatusCodes.Status201Created)]
+    public async Task<ActionResult<Comment>> Create(Comment comment)
+    {
+        int id = await service.CreateAsync(comment);
+        return CreatedAtAction(nameof(Read), new { id }, comment);
+    }
+
+    [HttpPut("{id:int}")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<IActionResult> Update(int id, Comment comment)
+    {
+        await service.UpdateAsync(id, comment);
+
+        return NoContent();
+    }
+
+    [HttpDelete("{id:int}")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<IActionResult> Delete(int id)
+    {
+        await service.DeleteAsync(id);
+
+        return NoContent();
+    }
+}
