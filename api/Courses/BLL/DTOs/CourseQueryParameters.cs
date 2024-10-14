e68b699f9c9b021042bc57a02f8583ce059e4bf6
maslov-maksym-serhiiovych-chnu <maslov.maksym@chnu.edu.ua>
2024-10-14 18:17:30 +0300

    Courses: create course query parameters for sorting, filtering and paging

---
+++ api/Courses/BLL/DTOs/CourseQueryParameters.cs
@@ -0,0 +1,9 @@
+﻿namespace BLL.DTOs;
+
+public class CourseQueryParameters
+{
+    public string? SearchTerm { get; set; }
+    public string? SortBy { get; set; }
+    public int PageIndex { get; set; }
+    public int PageSize { get; set; }
+}
