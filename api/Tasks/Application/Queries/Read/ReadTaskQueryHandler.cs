64cb2d0e5742e2f5ffb9580a21d3ea4303a4bbdc
maslov-maksym-serhiiovych-chnu <maslov.maksym@chnu.edu.ua>
2024-10-14 20:26:07 +0300

    Tasks: create task query handlers

---
+++ api/Tasks/Application/Queries/Read/ReadTaskQueryHandler.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Models;
+using Infrastructure.Persistence;
+using MediatR;
+
+namespace Application.Queries.Read;
+
+public class ReadTaskQueryHandler(TasksDbContext context) : IRequestHandler<ReadTaskQuery, TaskModel?>
+{
+    public async Task<TaskModel?> Handle(ReadTaskQuery request, CancellationToken cancellationToken)
+    {
+        TaskModel? task = await context.Tasks.FindAsync([request.Id], cancellationToken);
+        return task;
+    }
+}
---
+++ api/Tasks/Application/Queries/ReadAll/ReadAllTasksQueryHandler.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Models;
+using Infrastructure.Persistence;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Queries.ReadAll;
+
+public class ReadAllTasksQueryHandler(TasksDbContext context)
+    : IRequestHandler<ReadAllTasksQuery, IEnumerable<TaskModel>>
+{
+    public async Task<IEnumerable<TaskModel>> Handle(ReadAllTasksQuery request, CancellationToken cancellationToken)
+    {
+        var tasks = await context.Tasks.ToArrayAsync(cancellationToken);
+        return tasks;
+    }
+}
