666d748eb1da3b6234e67ba1fb995d7f484c2ca3
maslov-maksym-serhiiovych-chnu <maslov.maksym@chnu.edu.ua>
2024-10-14 21:46:44 +0300

    Tasks: create exception handler middleware

---
+++ api/Tasks/Presentation/Middlewares/ExceptionHandlerMiddleware.cs
@@ -0,0 +1,22 @@
+﻿using Domain.Exceptions;
+
+namespace Presentation.Middlewares;
+
+public class ExceptionHandlerMiddleware(RequestDelegate next, ILogger<ExceptionHandlerMiddleware> logger)
+{
+    public async Task InvokeAsync(HttpContext context)
+    {
+        try
+        {
+            await next(context);
+        }
+        catch (TaskNotFoundException exception)
+        {
+            string message = exception.Message;
+            context.Response.StatusCode = StatusCodes.Status404NotFound;
+            await context.Response.WriteAsJsonAsync(message);
+
+            logger.LogError(message);
+        }
+    }
+}
