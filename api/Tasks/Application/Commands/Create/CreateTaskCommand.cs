d2100c6e81acbd426430ccd45f7b7fda4ffa9429
maslov-maksym-serhiiovych-chnu <maslov.maksym@chnu.edu.ua>
2024-10-14 20:10:39 +0300

    Tasks: remove id from create task command

--- api/Tasks/Application/Commands/Create/CreateTaskCommand.cs
+++ api/Tasks/Application/Commands/Create/CreateTaskCommand.cs
@@ -2,4 +2,4 @@
 
 namespace Application.Commands.Create;
 
-public record CreateTaskCommand(int Id, string Name, string Description) : IRequest<int>;
+public record CreateTaskCommand(string Name, string Description) : IRequest<int>;
